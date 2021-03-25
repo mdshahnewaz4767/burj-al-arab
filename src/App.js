6657d2ab75cfbea964440c90955a6d03434621d9
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-26 00:28:14 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { createContext, useState } from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
+import Book from "./Components/Book/Book";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import LogIn from "./Components/LogIn/LogIn";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      {/* <p>Name: {loggedInUser.name}</p> */}
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <PrivateRoute path="/book/:bedType">
+            <Book />
+          </PrivateRoute>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/LogIn/LogIn.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+// Initialize Firebase
+if(firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+} 
+
+
+const LogIn = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
+    const handleGoogleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+            var credential = result.credential;
+            var token = credential.accessToken;
+            const {displayName, email} = result.user;
+            const signInUser = {name: displayName, email};
+            setLoggedInUser(signInUser);
+            history.replace(from);
+            console.log(token);
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        });
+    }
+
+    return (
+        <div style={{textAlign: 'center'}}>
+            {
+                !loggedInUser ? <h1>Already Sign In</h1> : 
+                <div>
+                    <h1>This is Login</h1>
+                    <button onClick={handleGoogleSignIn}>Google SignIn</button>
+                </div>
+                
+                
+            }
+        </div>
+    );
+};
+
+export default LogIn;
